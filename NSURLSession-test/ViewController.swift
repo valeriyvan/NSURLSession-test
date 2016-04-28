6d6608f42579901b387e8cb32762c229f910891a
Valeriy Van <val@w7software.com>
2016-04-28 11:07:28 +0300

    Initial Commit

---
+++ NSURLSession-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSURLSession-test
+//
+//  Created by Valeriy Van on 28.04.16.
+//  Copyright © 2016 Valeriy Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
